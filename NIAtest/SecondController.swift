68828e43d699e50203dbe4d10d3c8a7dcc30446b
Charles Arcodia <charlesarcodia@Charless-MacBook-Pro.local>
2017-03-13 13:52:13 -0400

    Added the second controller

---
+++ NIAtest/SecondController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondController.swift
+//  NIAtest
+//
+//  Created by Charles Arcodia on 3/13/17.
+//  Copyright © 2017 NoImpactApplicationsLLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondController : UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("second controller")
+    }
+}
