956d9259333cc2bf3142feb11eb5a26f5623d7a7
Charles Arcodia <charlesarcodia@Charless-MacBook-Pro.local>
2017-03-13 16:48:59 -0400

    This is the new branch.

---
+++ NIAtest/NewBranchFile.swift
@@ -0,0 +1,19 @@
+//
+//  NewBranchFile.swift
+//  NIAtest
+//
+//  Created by Charles Arcodia on 3/13/17.
+//  Copyright © 2017 NoImpactApplicationsLLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewBranch : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("new branch")
+    }
+}
